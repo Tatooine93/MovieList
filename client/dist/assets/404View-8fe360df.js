4349890d501945679c1c1e0ba25e3e63a58c255d
Antoine <nowakantoine93@gmail.com>
2025-10-07 20:44:04 -0600

    build

--- client/dist/assets/404View-8fe360df.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,c as t,a as c}from"./index-2e16db4b.js";const n={},s=c("h1",null,"404 Page Not Found",-1),a=[s];function _(r,l){return o(),t("main",null,a)}const i=e(n,[["render",_]]);export{i as default};
