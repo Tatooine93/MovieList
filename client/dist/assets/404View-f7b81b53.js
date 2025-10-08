64935ac10c1613b75a52d21d68553296d29d51ba
Antoine <nowakantoine93@gmail.com>
2025-10-07 20:30:37 -0600

    Revert "build"
    This reverts commit 970e8d4acde879daa7f62c97f8c59586f8bb46dc.

--- client/dist/assets/404View-f7b81b53.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,c as t,a as c}from"./index-a4f8bda1.js";const n={},s=c("h1",null,"404 Page Not Found",-1),a=[s];function _(r,l){return o(),t("main",null,a)}const i=e(n,[["render",_]]);export{i as default};
